e7961822fbff860c8d6340627e5573edfe76bcc0
Warren Clough <clough.warren@gmail.com>
2019-05-06 06:55:18 -0500

    added static data query and updated the search algorithm

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
 const express = require('express')
-const port = 3000
 const axios = require('axios')
+const port = 3000
+
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const Promise = require("bluebird");
 const bodyParser = require('body-parser');
+const { setupCache } = require('axios-cache-adapter');
+const moment = require('moment');
+
+let poesessid = '';
 /**
  * Variables
  */
 const baseUrl = 'https://www.pathofexile.com'
 
 /**
- * Interceptors
+ * Cache
  */
 
+// Create `axios-cache-adapter` instance
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000
+})
+
+// Create `axios` instance passing the newly created `cache.adapter`
+const api = axios.create({
+  adapter: cache.adapter
+})
+
+/**
+ * Interceptors
+ */
+api.interceptors.request.use((config) => {
+  config = {
+    ...config,
+    metaData: {
+      startTime: Date.now(),
+    },
+    headers: {
+      ...config.headers,
+      Cookie: `${config.headers.Cookie ? config.headers.Cookie : ''}POESESSID=${poesessid};`
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
+api.interceptors.response.use((response) => {
+  // Do something with response data
+  const duration = moment().diff(moment(response.config.metaData.startTime), 'milliseconds');
+  console.log(`axios ${response.config.method.toUpperCase()} ${response.config.url} ${duration}ms`);
+  return response;
+}, (error) => {
+  // Do something with response error
+  // console.error(error);
+  // const duration = moment().diff(moment(response.config.metaData.startTime), 'milliseconds');
+  // console.log(`axios ${response.config.method.toUpperCase()} ${response.config.url} ${duration}ms`);
+  return Promise.reject(error);
+})
 
 /**
  * Middlewares
@@ -24,21 +71,9 @@ app.use(cookieParser())
 app.use(bodyParser());
 
 app.use((req, res, next) => {
-  if(req.headers.poesessid) {
-    const poesessid = req.headers.poesessid;
-    axios.interceptors.request.use((config) => {
-      config = {
-        ...config,
-        headers: {
-          ...config.headers,
-          Cookie: `${config.headers.Cookie?config.headers.Cookie:''}POESESSID=${poesessid};`
-        }
-      }
-    
-      return config;
-    }, (error) => {
-      return Promise.reject(error);
-    });
+  if (req.headers.poesessid) {
+    poesessid = req.headers.poesessid;
+
   }
   next();
 })
@@ -65,38 +100,181 @@ app.get('/', (req, res) => {
   });
 
 })
-app.post('/get-item-value', (req, res) => {
+app.post('/get-similar-items', async (req, res) => {
 
   const item = req.body;
-  // console.log(item);
-  const itemSearchQuery = {
+  const leauge = req.param('leauge');
+
+  //get stats for query
+  const stats = await api.get(`${baseUrl}/api/trade/data/stats`).then(stats => stats.data);
+  // console.log(stats.result.length);
+  //find explicit mods
+  let explicitModFilters = []
+  if (item.explicitMods) {
+    explicitModFilters = stats.result
+      .find(statType => statType.label == 'Explicit')
+      .entries
+      .filter(stat => {
+        if(stat.text.match('\\+', '\\+')){
+          const test = stat.text.replace('+', '').replace(/#/g, '\\d+');
+          console.log(test);
+        }
+        let regexText = stat.text.replace('+', '').replace(/#/g, '\\d+')
+        regexText = `${regexText}$`;
+        const regex = new RegExp(regexText, 'g');
+        for (let i = 0; i < item.explicitMods.length; i++) {
+          if (item.explicitMods[i].match(regex)) {
+            return true;
+          };
+
+        }
+        return false;
+      })
+      .map(stat => ({ id: stat.id }));
+    // explicitModFilters.push(explicitModFilter);
+  }
+
+  const type = { type: item.typeLine };
+  let categoryFilter = {};
+  for (const key in item.category) {
+    if (item.category.hasOwnProperty(key)) {
+      let categoryGroup = key;
+      console.log(categoryGroup);
+      switch (key) {
+        case 'gems':
+          categoryGroup = 'gem'
+          break;
+        case 'accessories':
+          categoryGroup = 'accessory'
+          break;
+        case 'weapons':
+          categoryGroup = 'weapon'
+          break;
+        case 'maps':
+          categoryGroup = 'map'
+          break;
+        case 'jewels':
+          categoryGroup = 'jewel'
+          break;
+        case 'flasks':
+          categoryGroup = 'flask'
+          break;
+      }
+      categoryFilter = {
+        category: {
+          option: categoryGroup + (item.category[key].length > 0 ? "." + item.category[key][0] : "")
+        }
+      };
+      break;
+    }
+  }
+  let rairity = {}
+  switch (item.frameType) {
+    case 0:
+      rairity = {
+        rarity: {
+          option: "normal"
+        }
+      };
+
+      break;
+    case 1:
+      rairity = {
+        rarity: {
+          option: "magic"
+        }
+      };
+
+      break;
+    case 2:
+      rairity = {
+        rarity: {
+          option: "rare"
+        }
+      };
+      break;
+    case 3:
+      rairity = {
+        rarity: {
+          option: "unique"
+        }
+      };
+      break;
+    default:
+      break;
+  }
+
+  const itemLevel = {
+    filters: {
+      ilvl: {
+        min: item.ilvl
+      }
+    }
+  }
+  const tradeQuery = {
     query: {
       status: {
         option: "online"
       },
-      type: item.typeLine
+      stats: [
+        {
+          type: "and",
+          filters: [
+            ...explicitModFilters
+          ]
+        }
+      ],
+      filters: {
+        misc_filters: {
+          ...itemLevel
+        },
+        type_filters: {
+          filters: {
+            ...categoryFilter,
+            ...rairity
+          }
+        }
+      },
+      // ...type
     },
     sort: {
       price: "asc"
     }
   }
-  axios.post(
-    `${baseUrl}/api/trade/search/Synthesis`,
-    itemSearchQuery
-  ).then(itemSearchQueryResponse => {
-    if (itemSearchQueryResponse.data && itemSearchQueryResponse.data.result.length > 0) {
-      return axios.get(
-        `${baseUrl}/api/trade/fetch/${itemSearchQueryResponse.data.result[0]}?query=${itemSearchQueryResponse.data.id}`,
-        itemSearchQuery
-      ).then(fetchResponse => {
-        res.send(fetchResponse.data);
-      })
-    } else {
-      res.send([]);
+  console.log(JSON.stringify(tradeQuery));
+  console.log(JSON.stringify(item.category));
 
+  // get trade data summary
+  api.post(`${baseUrl}/api/trade/search/${leauge}`, tradeQuery).then(({ data: searchData }) => {
+    if (searchData.result.length > 0) {
+      const selectList = searchData.result
+        .filter((data, index) => index < 10)
+        .map(itemId => itemId)
+        .join(",")
+
+      // get the detail of trade data
+      return api.get(`${baseUrl}/api/trade/fetch/${selectList}?query=${searchData.id}`)
+        .then(({ data: tradeData }) => {
+          res.send({
+            ...tradeData,
+            link: `${baseUrl}/trade/search/${leauge}/${searchData.id}`
+          });
+        })
+        .catch((error) => {
+          // console.error(error)
+          res.status(error.response.status).json(error.response.data);
+        })
+    } else {
+      console.log('no data');
+      res.send({
+        link: `${baseUrl}/trade/search/${leauge}/${searchData.id}`
+      });
     }
   }).catch(error => {
-    res.send(error.response ? error.response.data : error);
+    // console.error(error)
+    res.send(error.response ? {
+      ...error.response.data,
+    } : error);
   })
 })
 
@@ -104,8 +282,8 @@ app.post('/get-item-value', (req, res) => {
 app.get('/get-items', (req, res) => {
   const { accountName, character } = req.query;
 
-  axios.get(
-    `${baseUrl}/character-window/get-items`,{
+  api.get(
+    `${baseUrl}/character-window/get-items`, {
       params: {
         accountName,
         character
@@ -127,17 +305,31 @@ app.get('/get-characters', (request, response) => {
     params.character = character;
   }
 
-  axios.get(
-    `${baseUrl}/character-window/get-characters`,{
+  api.get(
+    `${baseUrl}/character-window/get-characters`, {
       params
     }
   ).then(data => {
     response.send(data.data);
   }).catch(error => {
-    if(!error.response){
-       response.status(500).send(error);
-    } else{
+    if (!error.response) {
+      response.status(500).send(error);
+    } else {
       response.status(error.response.status).send(error.response.data);
+    }
+  });
+})
+
+
+// trade data
+app.get('/poe-static-data', (req, res) => {
+  api.get(`${baseUrl}/api/trade/data/static`).then(({ data }) => {
+    res.send(data.result);
+  }).catch(error => {
+    if (!error.response) {
+      res.status(500).send(error);
+    } else {
+      res.status(error.response.status).send(error.response.data);
     }
   });
 })
